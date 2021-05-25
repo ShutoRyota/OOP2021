0c6bc4d613d99689224af8219dc35c87cf8b4824
ShutoRyota <32005@ojs.ac.jp>
2021-05-25 10:20:28 +0900

    p52 リスト2.6 変換処理をクラス化

---
+++ DistanceConverter/FeetConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    class FeetConverter {
+
+        //メートルからフィート
+        public double MeterToFeet(double meter) {
+            return meter / 0.3048;
+        }
+
+        //フィートからメートルを求める
+        public double FeetToMeter(double feet) {
+            return feet * 0.3048;
+        }
+
+    }
+
+        
+}
--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -6,38 +6,30 @@ using System.Threading.Tasks;
 
 namespace DistanceConverter {
     class Program {
+
+        static FeetConverter fc = new FeetConverter();
+
         static void Main(string[] args) {
             if (args.Length >= 1 && args[0] == "-tom") {
                 PrintFeetToMeterList(1, 10);
             } else {
-                PrintMeterToFeetList(1,10);
+                PrintMeterToFeetList(1, 10);
             }
         }
 
         private static void PrintFeetToMeterList(int start, int stop) {
             for (int feet = start; feet <= stop; feet++) {
-                double meter = FeetToMeter(feet);
+                double meter = fc.FeetToMeter(feet);
                 Console.WriteLine("{0}ft = {1:0.0000}m", feet, meter);
             }
         }
          //↑出力↓
         private static void PrintMeterToFeetList(int start,int stop) {
             for (int meter = start; meter <= stop; meter++) {
-                double feet = MeterToFeet(meter);
+                double feet = fc.MeterToFeet(meter);
                 Console.WriteLine("{0}m = {1:0.0000}ft", meter, feet);
             }
         }
         
-        
-
-        //フィートからメートルを求める
-        private static double FeetToMeter(int feet) {
-            return feet * 0.3048;
-        }
-       
-        //メートルからフィート
-        private static double MeterToFeet(int meter) {
-            return meter / 0.3048;
-        }
     }
 }
