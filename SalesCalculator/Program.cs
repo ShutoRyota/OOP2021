642811e4f32f2005bd12b3d1a9f8700f38e6b70c
ShutoRyota <32005@ojs.ac.jp>
2021-05-28 14:03:49 +0900

    P77 完成

--- SalesCalculator/Program.cs
+++ SalesCalculator/Program.cs
@@ -8,8 +8,9 @@ using System.Threading.Tasks;
 namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
-            SalesCounter sales = new SalesCounter(@"Sales.csv");
-            Dictionary<string, int> amountPerSrore = sales.GetPerStoreSales();
+            var sales = new SalesCounter(@"Sales.csv");
+        var amountPerSrore = sales.GetPerStoreSales();
+
             foreach(KeyValuePair<string,int> obj in amountPerSrore) {
                 Console.WriteLine("{0} {1}", obj.Key, obj.Value);
             }
