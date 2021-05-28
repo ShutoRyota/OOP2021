9d3ee4cd155a2cd2d7a596a5f998b97272f25ff8
ShutoRyota <32005@ojs.ac.jp>
2021-05-28 15:42:18 +0900

    問題2.3

--- Exercise/Sale.cs
+++ Exercise/Sale.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise3 {
+namespace Exercise {
     class Sale {
         //店舗名
         public string ShopName { get; set; }
--- Exercise/SalesCounter.cs
+++ Exercise/SalesCounter.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exercise3 {
+namespace Exercise {
     //売上修正クラス
     class SalesCounter  {
 
