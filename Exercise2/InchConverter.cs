2a41cbf6c117143fe37876b47ab18ca2863c84f3
ShutoRyota <32005@ojs.ac.jp>
2021-05-28 15:14:37 +0900

    問題2.2

---
+++ Exercise2/InchConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    static class InchConverter {
+
+        public static void PrintResult(int start,int stop) {
+            for(int i = start; i <= stop; i++) {
+                Console.WriteLine("{0}m = {1:0.0000}in",i,MeterToinch(i));
+            }
+        }
+
+        public static double MeterToinch(double meter) {
+            double inch = meter * 0.0245;
+
+            return inch;
+        }
+    }
+}
