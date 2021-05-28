d00d9f7fcddee17d32d86dd9ea05de2bda19276b
ShutoRyota <32005@ojs.ac.jp>
2021-05-28 14:26:48 +0900

    問題2.1

---
+++ Exercise1/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Song {
+        private string Title;
+        private string ArtistName;
+        private int Length;
+
+        public Song(string title, string artistName, int length) {
+            Title = title;
+            ArtistName = artistName;
+            Length = length;
+        }
+    }
+}
