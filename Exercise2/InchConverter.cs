e743e5d6f0c892d470f85e05e3304ec1ea651da3
ShutoRyota <32005@ojs.ac.jp>
2021-06-01 10:25:00 +0900

    問題2.2

--- Exercise2/InchConverter.cs
+++ Exercise2/InchConverter.cs
@@ -7,16 +7,14 @@ using System.Threading.Tasks;
 namespace Exercise2 {
     static class InchConverter {
 
-        public static void PrintResult(int start,int stop) {
-            for(int i = start; i <= stop; i++) {
-                Console.WriteLine("{0}m = {1:0.0000}in",i,MeterToInch(i));
-            }
-        }
+        private const double ratio = 0.0254;
 
         public static double MeterToInch(double meter) {
-            double inch = meter * 0.0245;
-
-            return inch;
+            return meter /ratio;
+        }
+        
+        public static double InchToMeter(double inch) {
+            return inch * ratio;
         }
     }
 }
--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -7,7 +7,25 @@ using System.Threading.Tasks;
 namespace Exercise2 {
     class Program {
         static void Main(string[] args) {
-            InchConverter.PrintResult(1, 10);
+
+            if (args.Length >= 1 && args[0] == "-toi") {
+                PrintMeterToInchList(1, 10);
+            } else {
+                PrintInchToMeterList(1, 10);
+            }
+        }
+
+
+        public static void PrintMeterToInchList(int start, int stop) {
+            for (int i = start; i <= stop; i++) {
+                Console.WriteLine("{0}m = {1:0.0000}in", i, InchConverter.MeterToInch(i));
+            }
+        }
+        
+        public static void PrintInchToMeterList(int start, int stop) {
+            for (int i = start; i <= stop; i++) {
+                Console.WriteLine("{0}in = {1:0.0000}m", i, InchConverter.InchToMeter(i));
+            }
         }
     }
 }
