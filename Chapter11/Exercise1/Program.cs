b0760fce3682dc05414d094a0d2b4e4454f4b278
涼太 周藤 <32005@ojs.ac.jp>
2021-08-27 15:02:12 +0900

    P299 問題11.1

---
+++ Chapter11/Exercise1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+
+            var file = "Sample.xml";
+
+            Exercise1_1(file);
+            Console.WriteLine("-------");
+
+            Exercise1_2(file);
+            Console.WriteLine("-------");
+
+            Exercise1_3(file);
+            Console.WriteLine("-------");
+
+        }
+
+        private static void Exercise1_1(string file) {
+            var doc = XDocument.Load(file);
+            var sports = doc.Root.Elements().
+                         Select(x => new {
+                             Name = (string)x.Element("name"),
+                             Teammenbers = (int)x.Element("teammembers")
+                         });
+            foreach(var sport in sports) {
+                Console.WriteLine($"{sport.Name} {sport.Teammenbers}");
+            }
+        }
+
+        private static void Exercise1_2(string file) {
+            
+        }
+
+        private static void Exercise1_3(string file) {
+          
+        }
+    }
+}
