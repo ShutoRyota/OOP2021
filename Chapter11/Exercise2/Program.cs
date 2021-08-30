3f3601ffe64b81f1cca697a8cae5339b323f3463
涼太 周藤 <32005@ojs.ac.jp>
2021-08-30 16:12:47 +0900

    P300問題11.2未完成

---
+++ Chapter11/Exercise2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Exercise2 {
+    class Program {
+        static void Main(string[] args) {
+            var newfile = "nandoku";
+            var xdoc = XDocument.Load("11_2.xml");
+
+            var diffikanjis = xdoc.Root.Elements();
+
+            foreach(var moji in diffikanjis) {
+                var newdoc = new XElement("difficultkanji",
+                             new XElement("word", new XElement("kanji", moji.Element("kanji")),
+                             new XElement("yomi", moji.Element("yomi"))));
+                
+                newdoc.Add(newfile);
+                newdoc.Save(newfile);
+                
+            }
+            
+            
+            var newxdoc = XDocument.Load(newfile);
+            
+            
+            foreach(var kan in newxdoc.Root.Elements()) {
+                var kanji = kan.Element("kanji");
+                Console.WriteLine(kanji);
+            }
+            
+
+           
+        }
+    }
+}
